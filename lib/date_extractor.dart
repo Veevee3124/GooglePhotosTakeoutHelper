eaddec0623eb0504f99a284b187a2e0b27f06b49
Veevee3124 <levee684@gmail.com>
2025-05-17 09:50:39 -0700

    yrtyhe

--- lib/date_extractor.dart
+++ lib/date_extractor.dart
@@ -1,8 +1,3 @@
-import 'dart:io';
-
-export 'date_extractors/exif_extractor.dart';
-export 'date_extractors/guess_extractor.dart';
-export 'date_extractors/json_extractor.dart';
-
+r
 /// Function that can take a file and potentially extract DateTime of it
 typedef DateTimeExtractor = Future<DateTime?> Function(File);
--- lib/extras.dart
+++ lib/extras.dart
@@ -1,37 +1,4 @@
-import 'package:path/path.dart' as p;
-import 'package:unorm_dart/unorm_dart.dart' as unorm;
-
-import 'media.dart';
-
-const extraFormats = [
-  // EN/US - thanks @DalenW
-  '-edited',
-  '-effects',
-  '-smile',
-  '-mix',
-  // PL
-  '-edytowane',
-  // DE - thanks @cintx
-  '-bearbeitet',
-  // NL - thanks @jaapp
-  '-bewerkt',
-  // JA - thanks @fossamagna
-  '-編集済み',
-  // IT - thanks @rgstori
-  '-modificato',
-  // FR - for @palijn's problems <3
-  '-modifié',
-  // ES - @Sappstal report
-  '-ha editado',
-    // CA - @Sappstal report
-  '-editat',
-  // Add more "edited" flags in more languages if you want.
-  // They need to be lowercase.
-];
-
-/// Removes any media that match any of "extra" formats
-/// Returns count of removed
-int removeExtras(List<Media> media) {
+rredia> media) {
   final copy = media.toList();
   var count = 0;
   for (final m in copy) {
